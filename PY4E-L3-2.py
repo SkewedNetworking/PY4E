e35361b7a66736ddc3711cf18cb2ee10d894f62a
SkewedNetworking <157815340+SkewedNetworking@users.noreply.github.com>
2024-01-26 09:58:28 -0600

    PY4E - Lesson 2
    Lesson 2 - Exercise 1

---
+++ PY4E-L3-2.py
@@ -0,0 +1,9 @@
+# This first line is provided for you
+
+hrs = input("Enter Hours:")
+rte = input("Enter Rate:")
+hrs = float(hrs)
+rte = float(rte)
+pay = rte * hrs
+pay = str(pay)
+print("Pay: " + pay)
